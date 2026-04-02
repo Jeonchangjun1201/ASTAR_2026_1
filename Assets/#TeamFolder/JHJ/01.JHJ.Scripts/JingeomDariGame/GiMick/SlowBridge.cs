c5e4cc607d7bb4f925b4e41238566e40e8718277
hyeonjin0209 <jonmoshyeonmin@gmail.com>
2026-04-02 17:10:04 +0900

    feat : Slow,fall,default Brifge,  BridgeManager, Test Player

---
+++ Assets/#TeamFolder/JHJ/01.JHJ.Scripts/JingeomDariGame/GiMick/SlowBridge.cs
@@ -0,0 +1,26 @@
+﻿using JHJ.Scripts.Test.TestPlayer;
+using UnityEngine;
+
+namespace JHJ.JHJ.Scripts.GiMick
+{
+    public class SlowBridge : MonoBehaviour
+    {
+        [Header("Setting")]
+        [SerializeField] private float slowSpeed = 2f;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player") && collision.gameObject.TryGetComponent(out TestPlayerController player))
+            {
+                player.SetMoveSpeed(slowSpeed);
+            }
+        }
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player") && collision.gameObject.TryGetComponent(out TestPlayerController player))
+            {
+                player.ResetMoveSpeed();
+            }
+        }
+    }
+}
---
+++ Assets/#TeamFolder/JHJ/04.SO/InputReader.cs
@@ -0,0 +1,37 @@
+﻿using System; // Action을 사용하기 위해 필요
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static JHJControls;
+namespace JHJ.Test.TestPlayer
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "InputReSO/InputReader")]
+    public class InputReader : ScriptableObject, IPlayerActions
+    {
+        private JHJControls inputcontrols;
+        public event Action<Vector2> OnMoveEvent;
+        public Vector3 moveDir { get; private set; }
+
+        private void OnEnable()
+        {
+            if (inputcontrols == null)
+            {
+                inputcontrols = new JHJControls();
+                inputcontrols.Player.SetCallbacks(this);
+            }
+            inputcontrols.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputcontrols.Player.Disable();
+        }
+
+        public void OnMoveMent(InputAction.CallbackContext context)
+        {
+            moveDir = context.ReadValue<Vector2>();
+
+            if (context.performed || context.canceled)
+                OnMoveEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+}
