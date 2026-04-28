d7826530e7c8e4810891d15fd512336d25733258
hwani165 <seungyung.kang@gmail.com>
2026-04-28 23:17:37 +0900

    feat : Session

--- Assets/#TeamFolder/KSY/_Scripts/Client/Session.cs
+++ Assets/#TeamFolder/KSY/_Scripts/Client/Session.cs
@@ -1,4 +1,9 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
 
 namespace KSY.Client
 {
@@ -6,7 +11,174 @@ namespace KSY.Client
     {
         private Socket _socket;
         private int _disconnected = 0;
-        ReceiveBuffer _receiveBuffer;
+        private ReceiveBuffer _receiveBuffer = new ReceiveBuffer(1028);
 
+        private object _lock = new object();
+        private Queue<ArraySegment<byte>> _sendQueue = new Queue<ArraySegment<byte>>();
+        private List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+        private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+        private SocketAsyncEventArgs _receiveArgs = new SocketAsyncEventArgs();
+
+        public abstract void OnConnect(EndPoint endPoint);
+        public abstract int OnReceive(ArraySegment<byte> buffer);
+        public abstract void OnSend(int numOfBytes);
+        public abstract void OnDisconnected(EndPoint endPoint);
+
+        private void Clear()
+        {
+            lock (_lock)
+            {
+                _sendQueue.Clear();
+                _pendingList.Clear();
+            }
+        }
+
+        public void Start(Socket socket)
+        {
+            _socket = socket;
+            _socket.NoDelay = true;
+            _receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
+            RegisterReceive();
+        }
+
+        public void Send(ArraySegment<byte> sendBuff)
+        {
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pendingList.Count == 0)
+                    RegisterSend();
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (Interlocked.Exchange(ref _disconnected, 1) == 1)
+                return;
+            OnDisconnected(_socket.RemoteEndPoint);
+            _receiveArgs.Dispose();
+            _sendArgs.Dispose();
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+            Clear();
+        }
+
+        void RegisterSend()
+        {
+            if (_disconnected == 1)
+                return;
+
+            while (_sendQueue.Count > 0)
+            {
+                ArraySegment<byte> buff = _sendQueue.Dequeue();
+                _pendingList.Add(buff);
+            }
+            _sendArgs.BufferList = _pendingList;
+
+            try
+            {
+                bool pending = _socket.SendAsync(_sendArgs);
+                if (pending == false)
+                    OnSendCompleted(null, _sendArgs);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"RegisterSend Failed {e}");
+            }
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();
+
+                        OnSend(_sendArgs.BytesTransferred);
+
+                        if (_sendQueue.Count > 0)
+                            RegisterSend();
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log($"SendFailed: {e}");
+                    }
+                }
+                else
+                {
+                    Debug.Log($"SendError: {args.SocketError}");
+                    Disconnect();
+                }
+            }
+        }
+
+        void RegisterReceive()
+        {
+            if (_disconnected == 1)
+                return;
+
+            _receiveBuffer.Clean();
+            ArraySegment<byte> segment = _receiveBuffer.WriteSegment;
+            _receiveArgs.SetBuffer(segment.Array, segment.Offset, segment.Count);
+
+            try
+            {
+                bool pending = _socket.ReceiveAsync(_receiveArgs);
+                if (pending == false)
+                    OnReceiveCompleted(null, _receiveArgs);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"RegisterRecv Failed {e}");
+            }
+        }
+
+        void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
+                try
+                {
+                    // Write 커서 이동
+                    if (_receiveBuffer.OnWrite(args.BytesTransferred) == false)
+                    {
+                        Disconnect();
+                        return;
+                    }
+
+                    // 컨텐츠 쪽으로 데이터를 넘겨주고 얼마나 처리했는지 받는다
+                    int processLen = OnReceive(_receiveBuffer.ReadSegment);
+                    if (processLen < 0 || _receiveBuffer.DataSize < processLen)
+                    {
+                        Disconnect();
+                        return;
+                    }
+
+                    // Read 커서 이동
+                    if (_receiveBuffer.OnRead(processLen) == false)
+                    {
+                        Disconnect();
+                        return;
+                    }
+
+                    RegisterReceive();
+                }
+                catch (Exception e)
+                {
+                    Debug.Log($"RecvFailed: {e}");
+                }
+            }
+            else
+            {
+                Debug.Log($"RecvError: {args.SocketError}");
+                Disconnect();
+            }
+        }
     }
 }
