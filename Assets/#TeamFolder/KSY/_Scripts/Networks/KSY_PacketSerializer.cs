c9145f979c276b56c2b300ba2a539e52eecfa0fa
hwani165 <seungyung.kang@gmail.com>
2026-05-06 23:28:52 +0900

    feat : Packages : MemoryPack

--- Assets/#TeamFolder/KSY/_Scripts/Networks/KSY_PacketSerializer.cs
+++ Assets/#TeamFolder/KSY/_Scripts/Networks/KSY_PacketSerializer.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using MemoryPack;
 
 namespace KSY.Networks
 {
-    public class KSY_PacketSerializer 
+    public class KSY_PacketSerializer
     {
         public static class Builder
         {
@@ -16,11 +17,26 @@ namespace KSY.Networks
                     packetIDMap = new Dictionary<Type, ushort>(),
                     factories = new Dictionary<ushort, Func<ArraySegment<byte>, KSY_IPacket>>()
                 };
-                Type[] array = (from t in assemblies.SelectMany((Assembly a) => a.GetType())
-                              where typeof(KSY_IPacket).IsAssignableFrom(t)
-                              where t.IsDefined(typeof(PacketAttribute)), inherit: false)
-                              where !t.IsAbstract && !t.IsInterface
-                              select t).ToArray();
+                Type[] array = (from t in assemblies.SelectMany((Assembly a) => a.GetTypes())
+                                where typeof(KSY_IPacket).IsAssignableFrom(t)
+                                where t.IsDefined(typeof(KSY_PacketAttribute), inherit: false)
+                                where t.IsDefined(typeof(MemoryPackableAttribute), inherit: false)
+                                where !t.IsAbstract && !t.IsInterface
+                                select t).ToArray();
+                foreach (Type packetType in array)
+                {
+                    KSY_PacketAttribute customAttribute = packetType.GetCustomAttribute<KSY_PacketAttribute>(inherit: false);
+                    if(customAttribute != null)
+                    {
+                        packetSerializer.packetIDMap[packetType] = customAttribute.PacketID;
+                        packetSerializer.factories[customAttribute.PacketID] = (ArraySegment<byte> packetData) => CreatePacket(packetType, packetData);
+                    }
+                }
+            }
+
+            private static KSY_IPacket CreatePacket(Type packetType, ArraySegment<byte> packetData)
+            {
+                return MemoryPackSerializer.Deserialize(packetType, packetData) as KSY_IPacket;
             }
         }
 
@@ -31,13 +47,13 @@ namespace KSY.Networks
 
         }
 
-        public ArrayPoolBufferWriter Seralize(KSY_IPacket packet)
-        {
-            if(packet == null)
-            {
-                throw new ArgumentNullException("packet");
-            }
-        }
+        //public ArrayPoolBufferWriter Seralize(KSY_IPacket packet)
+        //{
+        //    if (packet == null)
+        //    {
+        //        throw new ArgumentNullException("packet");
+        //    }
+        //}
     }
 }
 
