e89ae2be89878e120d1b9b87d9f3ed48c6f89dfb
hwani165 <seungyung.kang@gmail.com>
2026-05-07 08:48:37 +0900

    feat : KSY_DIContainer

---
+++ Assets/#TeamFolder/KSY/_Scripts/Networks/NetworkObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KSY.Networks
+{
+    public class NetworkObject : KSY_IDIContainer, IAsyncDisposable
+    {
+        private readonly KSY_DIContainer diContainer;
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInstance GetInstance<TInstance>() where TInstance : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetInstance(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
