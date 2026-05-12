20417fdf9b954d540393fbac7f14d26e4a803c45
hwani165 <seungyung.kang@gmail.com>
2026-05-12 17:00:21 +0900

    feat : KSY_GameInstance

--- Assets/#TeamFolder/KSY/_Scripts/Shared/KSY_GameInstance.cs
+++ Assets/#TeamFolder/KSY/_Scripts/Shared/KSY_GameInstance.cs
@@ -1,10 +1,12 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 namespace KSY.Shared
 {
     public class KSY_GameInstance : MonoBehaviour
     {
-        public static EPlayMode
+        public static KSY_EPlayMode PlayMode = KSY_EPlayMode.None;
+        public static KSY_DataTableManager DataTableManager = null;
     }
 }
 
