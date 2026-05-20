f3df4984cc19767f7edafe4f1c4fc20c7e421f5a
Tangent_09 <tangent09mail@gmail.com>
2026-05-20 19:25:25 +0900

    feat: title ui

--- Assets/#TeamFolder/PYH/02.Scripts/MiniGame/Manager/MiniGameManager.cs
+++ Assets/#TeamFolder/PYH/02.Scripts/MiniGame/Manager/MiniGameManager.cs
@@ -7,26 +7,18 @@ namespace PYH.Manager
     {
         [SerializeField] private AbstractMiniGame miniGame;
         private IMiniGame mini;
-        [SerializeField] private TimeManager timeManager;
         [field: SerializeField] public MiniGameType CurrentMiniGameType { get; private set; }
 
         public void Awake()
         {
             mini = miniGame as IMiniGame;
-            Debug.Assert(timeManager != null, "TimeManager is NULL!");
-
-            timeManager.OnTickEndEvent.AddListener(mini!.GameEnd);
-
             mini.Initialize(); //BUG
-            timeManager.Initialize();
         }
 
         public void OnMiniGameEndHandler()
         {
             Time.timeScale = 0;
-
             mini.OnMiniGameEndEvent.RemoveListener(OnMiniGameEndHandler);
-            timeManager.OnTickEndEvent.RemoveListener(mini.GameEnd);
 
             Debug.Log("Game End.");
         }
