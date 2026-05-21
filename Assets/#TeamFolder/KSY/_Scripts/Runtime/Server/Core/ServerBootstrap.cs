822ae971024d5bc5cd842d39be24901e54a8c94c
hwani165 <seungyung.kang@gmail.com>
2026-05-21 17:40:43 +0900

    Rename : ClientDefine -> GameDefine

--- Assets/#TeamFolder/KSY/_Scripts/Runtime/Server/Core/ServerBootstrap.cs
+++ Assets/#TeamFolder/KSY/_Scripts/Runtime/Server/Core/ServerBootstrap.cs
@@ -28,7 +28,7 @@ namespace KSY.Servers
             gameServer.Initialize(gameManager, dataTableManager);
             gameServer.Listen(port);
 
-            await SceneManager.LoadSceneAsync(inGameSceneName, LoadSceneMode.Single);
+            await SceneManager.LoadSceneAsync(ClientDefine, LoadSceneMode.Single);
         }
     }
 }
--- Assets/#TeamFolder/KSY/_Scripts/Runtime/Shared/Core/TestDefine.cs
+++ Assets/#TeamFolder/KSY/_Scripts/Runtime/Shared/Core/TestDefine.cs
@@ -1,6 +1,6 @@
-namespace KSY.Clients
+namespace KSY.Shared
 {
-    public static class ClientDefine
+    public static class TestDefine
     {
         static public string TEST_LOAD_SCENE_NAME = "KSY_InGame";
     }
