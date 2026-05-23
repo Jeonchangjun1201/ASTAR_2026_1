b6fe060f891169577832776d02b802440a1042eb
kimdohyun <james49951215@gmail.com>
2026-05-23 18:22:09 +0900

    feat : LopeGame

--- Assets/#TeamFolder/KDH/01.Code/SoccerGame/Manager/CountDown.cs
+++ Assets/#TeamFolder/KDH/01.Code/SoccerGame/Manager/CountDown.cs
@@ -3,43 +3,53 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
-public class CountDown : MonoBehaviour
+namespace _TeamFolder.KDH._01.Code.SoccerGame.Manager
 {
-    [Header("UI")]
-    [SerializeField] private TextMeshProUGUI text;
-    [SerializeField] private float displayDuration = 1f;
-    [SerializeField] private float startDuration = 0.5f; // Start! 표시 시간
-    [SerializeField] private GameObject[] stopObj;
-
-    public static event Action OnCountdownFinished;
-
-    private void Start()
+    public class CountDown : MonoBehaviour
     {
-        StartCoroutine(StartCountdown());
-    }
+        [Header("UI")]
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private float displayDuration = 1f;
+        [SerializeField] private float startDuration = 0.5f;
+        [SerializeField] private GameObject[] stopObj;
 
-    private IEnumerator StartCountdown()
-    {
-        foreach (var obj in stopObj)
-            if (obj != null) obj.SetActive(false);
+        public static event Action OnCountdownFinished;
 
-        // 3, 2, 1은 displayDuration으로
-        string[] countSteps = { "3", "2", "1" };
-        foreach (string step in countSteps)
+        private void Start()
         {
-            text.text = step;
-            yield return new WaitForSeconds(displayDuration);
+            RestartCountdown();
         }
 
-        // Start!만 짧게
-        text.text = "Start!";
-        yield return new WaitForSeconds(startDuration);
+        public void RestartCountdown()
+        {
+            StopAllCoroutines();
+            text.gameObject.SetActive(true);
+            StartCoroutine(StartCountdown());
+        }
 
-        text.gameObject.SetActive(false);
+        private IEnumerator StartCountdown()
+        {
+            // 카운트다운 중 오브젝트 비활성화
+            foreach (var obj in stopObj)
+                if (obj != null) obj.SetActive(false);
 
-        foreach (var obj in stopObj)
-            if (obj != null) obj.SetActive(true);
+            string[] countSteps = { "5", "4", "3", "2", "1" };
+            foreach (string step in countSteps)
+            {
+                text.text = step;
+                yield return new WaitForSeconds(displayDuration);
+            }
 
-        OnCountdownFinished?.Invoke();
+            text.text = "Start!";
+            yield return new WaitForSeconds(startDuration);
+
+            text.gameObject.SetActive(false);
+
+            // 카운트다운 끝나면 오브젝트 활성화
+            foreach (var obj in stopObj)
+                if (obj != null) obj.SetActive(true);
+
+            OnCountdownFinished?.Invoke();
+        }
     }
 }
