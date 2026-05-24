10e95992de0595b5655b8cdfdd48aac1c8c51592
Tangent_09 <tangent09mail@gmail.com>
2026-05-24 14:01:51 +0900

    feat: Requested Uis

---
+++ Assets/#TeamFolder/PYH/02.Scripts/UI/Event/DigitalClockUiTimeSetEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace _TeamFolder.PYH._02.Scripts.UI.Event
+{
+    public class DigitalClockUiTimeSetEvent
+    {
+        public int SEC { get; private set; }
+
+        public DigitalClockUiTimeSetEvent(int sec)
+        {
+            SEC = sec;
+        }
+    }
+}
