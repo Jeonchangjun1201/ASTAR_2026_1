1cbf94c4bf1d40862eb10c3b6517de39fb592ae4
basicallybac <gongdohyeon14@gmail.com>
2026-05-25 22:37:39 +0900

    feat: TOW Animation finish
    player pull rope

--- Assets/#TeamFolder/BFS/BFS/Scripts/TOWScripts/Managers/TOWGameManager.cs
+++ Assets/#TeamFolder/BFS/BFS/Scripts/TOWScripts/Managers/TOWGameManager.cs
@@ -15,13 +15,19 @@ namespace BFS
         private RopeTOW _rope;
         private TOWScoreManager _scoreManager;
         private TOWGameOverManager _gameOverManager;
+
+        private int _idleHash;
+        private int _pullHash;
         private void Awake()
         {
+            _idleHash = Animator.StringToHash("IDLE");
+            _pullHash = Animator.StringToHash("PULL");
             playerList = GetComponentsInChildren<AbstractTeamTOW>();                                                                    // Collect players attached to game manager // 게임 매니저에서 플레이어들을 모으고
             int cnt = 0;
             foreach (RopePull rp in playerList)                                                                                         // Initialize each player; apply team, and player script attached to them // 모든 플레이어들을 이니셜라이즈하고, 팀을 부여함
             {
                 rp.Initialize(cnt++ % 2 == 0 ? PlayerTeamTOW.TEAMONE : PlayerTeamTOW.TEAMTWO, rp.GetComponentInParent<PlayerTOW>());
+                rp.TOWAnimator.Play(_idleHash);
             }
             _scoreManager = new TOWScoreManager(playerList, uiManager);                                                                            // Constructor; sends playerList to ScoreManager then instantiates // 생성자로 스코어 매니저를 만들고 매개변수로 플레이어 리스트 보냄
             _gameOverManager = new TOWGameOverManager(qteManager, _scoreManager, uiManager);                                            // Constructpr // 생성자
@@ -51,6 +57,10 @@ namespace BFS
         }
         public void StartGame()
         {
+            foreach(RopePull rp in playerList)
+            {
+                rp.TOWAnimator.Play(_pullHash);
+            }
             uiManager.ChangeText(uiManager.GameOverText, "START!", 3);
             qteManager.StartMinigame();
             DigitalClockUiStartEvent startEv = new DigitalClockUiStartEvent();
@@ -58,6 +68,10 @@ namespace BFS
         }
         public void EndGame()
         {
+            foreach (RopePull rp in playerList)
+            {
+                rp.TOWAnimator.Play(_idleHash);
+            }
             SetTimerFalse();
             _gameOverManager.EndGame();
         }
